d854f03f6f61452cc41809bbb66a4333b4ad0f3e
Fadil-Tao <hadadfadilah2354@gmail.com>
2023-07-01 18:28:50 +0800

    Maybe the last

---
+++ script.js
@@ -0,0 +1,38 @@
+async function getRandomUser(){ 
+    const response = await fetch('https://randomuser.me/api/');
+    const data = await response.json();
+    const user = data.results[0];
+    console.log(user);
+    displayuser(user);  
+
+}
+function displayuser(user){
+    const name = document.getElementById('name');
+    const email = document.getElementById("email");
+    const phone = document.getElementById("phone");
+    const location = document.getElementById("location");
+    const image = document.getElementById("image");
+
+    name.innerText = `${user.name.title} ${user.name.first} ${user.name.last}` ;
+    email.innerText= `${user.email}`
+    phone.innerText = `${user.phone}`
+    location.innerText = `${user.location.city} , ${user.location.country}`
+    image.setAttribute('src',`${user.picture.large}`)
+
+}
+
+function showLoadingPopup() {
+
+    var loadingPopup = document.getElementById("loadingPopup");
+    loadingPopup.style.display = "block";
+    setTimeout(function() {
+      loadingPopup.style.display = "none";
+    }, 1000);
+    getRandomUser()
+  }
+
+function clicked() {
+    showLoadingPopup();
+    getRandomUser() ;
+}
+getRandomUser();
